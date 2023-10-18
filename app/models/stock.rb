252f604b8a844de14b9e8d417018bb4b9c1184ff
jubin404 <jubin.jose@terrificminds.com>
2023-10-18 13:36:16 +0530

    encrypt and store iex api keys

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,8 +2,8 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: ENV['IEX_PUBLIC_KEY'],
-      secret_token: ENV['IEX_SECRET_KEY'],
+      publishable_token: Rails.application.credentials.iex_client[:public_key],
+      secret_token:  Rails.application.credentials.iex_client[:secret_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
 
