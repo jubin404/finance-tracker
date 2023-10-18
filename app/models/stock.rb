e018dd1aa8b9df7146cdb0a9aff71917b8c0d51e
jubin404 <jubin.jose@terrificminds.com>
2023-10-18 12:58:11 +0530

    add stock model and price look up action

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: ENV['IEX_PUBLIC_KEY'],
+      secret_token: ENV['IEX_SECRET_KEY'],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+
+    client.price(ticker_symbol)
+  end
 end
